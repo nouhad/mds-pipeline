a9bd2927b56b1272bafb8fa170d9057a2796eb7c
nouhad <nouhad@gmail.com>
2026-04-07 23:07:45 +1200

    day01: add task_03_loop_create_objects

---
+++ exercises/day01/task_03_loop_create_objects.py
@@ -0,0 +1,18 @@
+# 3D2G04A_3DScripting: Day 01
+# Task 03: Loop and Create Objects
+# Description: Use a loop to create 5 cubes, space them apart along the X axis, and print each transform node name.
+
+import maya.cmds as cmds
+
+
+# Task 1: Use a loop to create 5 cubes.
+# Task 2: Move each cube along the X axis so they do not overlap.
+# Task 3: Print the transform node name of each cube.
+
+cubes = []
+
+for i in range(5):
+    cube_transform = cmds.polyCube()[0]
+    cmds.move(i * 3, 0, 0, cube_transform)
+    cubes.append(cube_transform)
+    print("Created:", cube_transform)
